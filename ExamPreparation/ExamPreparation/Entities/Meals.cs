17e4e1ebd16971b86d577a82481e6de0130e35cc
Dylan <dylan.folens@student.howest.be>
2019-06-07 12:15:54 +0200

    Added database connection

---
+++ ExamPreparation/ExamPreparation/Entities/Meals.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExamPreparation.Entities
+{
+    public partial class Meals
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Img { get; set; }
+        public int? Book { get; set; }
+        public int? Calories { get; set; }
+        public int? Servings { get; set; }
+        public string Type { get; set; }
+        public int? Price { get; set; }
+        public string Cook { get; set; }
+        public int? Quantity { get; set; }
+    }
+}
